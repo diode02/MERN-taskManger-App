2464aba87859d67b3e352a6a8bc7912bb32c6ba2
Muhammad Waqas Ilyas <diode02@hotmail.com>
2020-09-28 14:51:18 +0500

    facebok like integration stash resolved

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Header from "./components/header/header-com";
 import DashBoard from "./pages/dashboard/dashboard-com";
 import SignInSignUp from "./pages/sign-in-sign-up/sign-in-sign-up.com";
-
+import "./App.css";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
--- client/src/pages/dashboard/dashboard-com.jsx
+++ client/src/pages/dashboard/dashboard-com.jsx
@@ -1,29 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
-import { LinkContainer } from "react-router-bootstrap";
+import { Button } from "primereact/button";
 
+import { InputText } from "primereact/inputtext";
+import { Dropdown } from "primereact/dropdown";
+import { InputTextarea } from "primereact/inputtextarea";
+
+import { Accordion, AccordionTab } from "primereact/accordion";
+
+import "./test.css";
 import { Redirect } from "react-router";
-import CustomButton from "../../components/custom-button/custom-button.com";
+import { TabView, TabPanel } from "primereact/tabview";
+import UserNameChange from "../../components/userNameChange/userNameChange-com";
+import EmailChange from "../../components/emailChange/emailChange-com";
+
 const Dashboard = () => {
   const currentUser = useSelector((state) => state.user.currentUser);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   if (!currentUser) {
     return <Redirect to="/login" />;
   }
   return (
-    <div className="Settings">
-      <LinkContainer to="/dashboard/email">
-        <CustomButton block bsSize="large">
-          Change Email
-        </CustomButton>
-      </LinkContainer>
-      <LinkContainer to="/dashboard/password">
-        <CustomButton block bsSize="large">
-          Change Password
-        </CustomButton>
-      </LinkContainer>
-      <hr />
-    </div>
+    <TabView
+      activeIndex={activeIndex}
+      onTabChange={(e) => setActiveIndex(e.index)}
+    >
+      <TabPanel header="Profile">
+        <div
+          style={{
+            display: "flow-root",
+          }}
+        >
+          <UserNameChange />
+          <EmailChange />
+        </div>
+      </TabPanel>
+      <TabPanel header="Change Password">
+        <div className="p-fluid p-formgrid p-grid">
+          <div className="p-field p-col-12 p-md-6">
+            <label htmlFor="firstname6">Firstname</label>
+            <InputText id="firstname6" type="text" />
+          </div>
+          <div className="p-field p-col-12 p-md-6">
+            <label htmlFor="lastname6">Lastname</label>
+            <InputText id="lastname6" type="text" />
+          </div>
+          <div className="p-field p-col-12">
+            <label htmlFor="address">Address</label>
+            <InputTextarea id="address" type="text" rows="4" />
+          </div>
+          <div className="p-field p-col-12 p-md-6">
+            <label htmlFor="city">City</label>
+            <InputText id="city" type="text" />
+          </div>
+          <div className="p-field p-col-12 p-md-3">
+            <label htmlFor="state">State</label>
+            <Dropdown inputId="state" placeholder="Select" optionLabel="name" />
+          </div>
+          <div className="p-field p-col-12 p-md-3">
+            <label htmlFor="zip">Zip</label>
+            <InputText id="zip" type="text" />
+          </div>
+        </div>
+      </TabPanel>
+      <TabPanel header="Change Avatar">Content III</TabPanel>
+    </TabView>
   );
 };
 
